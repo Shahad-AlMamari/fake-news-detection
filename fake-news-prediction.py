0acae0a97c5bb2be338fe454abdecb53ad1675de
Shahad-AlMamari <39050639+Shahad-AlMamari@users.noreply.github.com>
2021-11-11 12:33:54 +0400

    Update fake-news-prediction.py
    remove unused imports, change the classifier model

--- fake-news-prediction.py
+++ fake-news-prediction.py
@@ -4,17 +4,9 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.text import Text
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score
 import joblib
-import pickle
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
 import streamlit as st
-from lime.lime_text import LimeTextExplainer
-import streamlit.components.v1 as components
-import numpy as np
 from nltk.stem.porter import PorterStemmer
 
 
@@ -43,27 +35,30 @@ def preprocessor(text):
     return corpus
 
 # Loading the Models    
-classifier_model = joblib.load('RF_model.pkl')
+classifier_model = joblib.load('RF_classifier.pkl')
+vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1,3),vocabulary=joblib.load(open("tfidf_features.pkl","rb")))
 
 # Generating and Displaying Predictions
-def classify_news(cmodel, news):
+def classify_news(fmodel,cmodel, news):
 
     #tfidf     
-    vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1,3),vocabulary=joblib.load(open("tfidf_features.pkl","rb")))
-    tfidf = vectorizer.fit_transform(preprocessor(news))
+    #tfidf = vectorizer.fit_transform(preprocessor(news))
     #rf classifiation
-    label = classifier_model.predict(tfidf)[0]
+    #label = classifier_model.predict(tfidf)[0]
+    tfidf = fmodel.fit_transform(preprocessor(news))
+    #rf classifiation
+    label = cmodel.predict(tfidf)[0]
     if label == 1:
         prediction = 'Real'
     elif label == 0:
         prediction = 'Fake' 
-        
+    pred = "label", label    
     return {'label': label}
+    #return pred
     
 # output the model’s predictions as a dictionary
 if news_text != '':
     #result = preprocessor(news_text)    
-    result = classify_news(classifier_model, news_text)
+    result = classify_news(vectorizer, classifier_model, news_text)
 
     st.write(result)
-
